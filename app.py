df228cc0343f78f32be0d934a3bd03c152705d1b
marinaP <mar.pavlovic@gmail.com>
2023-09-12 11:12:55 +0200

    Update About page

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def login():
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
-    return render_template("login.html")
+    return render_template("journal/journal.html")
 
 
 @app.route("/logout")
